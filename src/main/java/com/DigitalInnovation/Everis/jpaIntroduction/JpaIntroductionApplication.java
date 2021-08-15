24d7e1217015fb54c268ce41d2e417587109c8e2
Ezau Martins <ezaumateus97@gmail.com>
2021-08-15 15:15:41 -0400

    Initial commit

---
+++ src/main/java/com/DigitalInnovation/Everis/jpaIntroduction/JpaIntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.DigitalInnovation.Everis.jpaIntroduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaIntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaIntroductionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DigitalInnovation/Everis/jpaIntroduction/JpaIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DigitalInnovation.Everis.jpaIntroduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaIntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
